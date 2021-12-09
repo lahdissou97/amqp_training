421ddf4d7d93b33f94d23fdaaa40f0d06ee0ccda
Ahmed Lahdissou <lahdissouahmed1997@gmail.com>
2021-12-09 19:55:20 +0100

    lastcommit2

--- client/src/main/java/fr/lernejo/chat/Launcher.java
+++ client/src/main/java/fr/lernejo/chat/Launcher.java
@@ -1,28 +1,29 @@
 package fr.lernejo.chat;
 
-import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.util.Scanner;
 
 @SpringBootApplication
-
 public class Launcher {
-
     public static void main(String[] args) {
-
-        Scanner scanner = new Scanner(System.in);
-        String input;
-        boolean start = true;
-        System.out.println("Saisissez un message, nous vous l'enverrons (q pour quitter)");
-        input = scanner.nextLine();
-
-        while(!(input.equals("q"))) {
-            System.out.println("message envoyé, Saisissez un message, nous vous l'enverrons (q pour quitter)");
-            input = scanner.nextLine();
-
+        SpringApplication.run(Launcher.class,args);
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(Launcher.class);
+        RabbitTemplate template = ctx.getBean(RabbitTemplate.class);
+        Scanner sc = new Scanner(System.in);
+        String line;
+        System.out.println("Input a message, we will send it for you (q for quit)");
+        while (true){
+            line = sc.nextLine();
+            if (line.equals("q")){
+                System.out.println("bye");
+                ctx.close();
+                break;}
+            System.out.println("Message sent. Input a message, we will send it for you");
+            template.convertAndSend("","chat_messages",line);
         }
     }
 }
